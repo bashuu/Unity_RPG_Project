f710e1433cac9b85b637edf6d43a29cae8f5600f
Sumyakhuu <sumyahuu@gmail.com>
2021-06-21 13:22:01 +0800

    Added simple movement, testGournds  and folders
    folders
    -scirpts
    -materials

---
+++ Simple_RPG/Assets/Scripts/MouseController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
